8968420160531670a2f907451d5039c179f92437
2lavea02 <2lavea02@solent.ac.uk>
2019-06-10 16:44:03 +0100

    Initial commit

---
+++ app/src/main/java/uk/ac/solent/resitassignment/MainActivity.kt
@@ -0,0 +1,59 @@
+package uk.ac.solent.resitassignment
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.SENSOR_DELAY_GAME
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
+    lateinit var image: ImageView
+    lateinit var accel: Sensor
+    lateinit var magnetSet: Sensor
+
+    var currentDegree = 0.0f
+    var lastAccel = FloatArray(3)
+    var lastMagnet = FloatArray(3)
+    var lastAccelerometerSet = false
+    var lastMagnetometerSet = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image = findViewById(R.id.imageViewCompass) as ImageView
+
+        val sMgr = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accel = sMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magnetSet = sMgr.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+
+        sMgr.registerListener(this, accel, SensorManager.SENSOR_DELAY_UI)
+    }
+    override fun onResume(){
+        super.onResume()
+
+        sensorManager.registerListener(this, accel, SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, magnetSet, SENSOR_DELAY_GAME)
+    }
+    override fun onPause(){
+        super.onPause()
+        sensorManager.unregisterListener(this, accel)
+        sensorManager.unregisterListener(this, magnetSet)
+
+    }
+    /* https://github.com/andreas-mausch/compass/blob/master/app/src/main/java/de/neonew/compass/MainActivity.kt */
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+    }
+}
